ff532b138df3dd9bb6334e189c44f120209cd109
lchevallier-teledyne <124684007+lchevallier-teledyne@users.noreply.github.com>
2023-02-13 15:25:14 +0100

    analog_gain set/get + restructuratio

---
+++ README.md
@@ -0,0 +1,3 @@
+The purpose of this code is to provide a simple example of using the V4L2 driver of the Topaz module in C.
+
+This code is composed of example functions to control the V4L2 drive and an example of sequential image capture.
--- test.c
+++ test.c
@@ -16,17 +16,20 @@
 #include <string.h>
 #include <fcntl.h>
 #include <errno.h>
+#include <stdint.h>
 #include <sys/ioctl.h>
 #include <sys/types.h>
 #include <sys/time.h>
 #include <sys/mman.h>
 #include <linux/videodev2.h>
 #include <libv4l2.h>
+#include <unistd.h>
 
 #define CLEAR(x) memset(&(x), 0, sizeof(x))
 
-struct buffer {
-        void   *start;
+struct buffer
+{
+        void *start;
         size_t length;
 };
 
@@ -34,136 +37,65 @@ static void xioctl(int fh, int request, void *arg)
 {
         int r;
 
-        do {
-                r = /*v4l2_*/ioctl(fh, request, arg);
+        do
+        {
+                r = ioctl(fh, request, arg);
         } while (r == -1 && ((errno == EINTR) || (errno == EAGAIN)));
 
-        if (r == -1) {
+        if (r == -1)
+        {
                 fprintf(stderr, "error %d, %s\\n", errno, strerror(errno));
                 exit(EXIT_FAILURE);
         }
 }
 
-void set_analog_gain(int fd,int value)
+int64_t get_analog_gain(int fd)
 {
-           int fd = -1;
-
-        fd = /*v4l2_*/open("/dev/video0", O_RDWR | O_NONBLOCK, 0);
-        if (fd < 0) {
-                perror("Cannot open device");
-                exit(EXIT_FAILURE);
-        }
-
-   struct v4l2_ext_controls ecs;
-   struct v4l2_ext_control ec;
-   memset(&ecs, 0, sizeof(ecs));
-   memset(&ec, 0, sizeof(ec));
-   ec.id = 0x009a206f;
-   ecs.controls = &ec;
-   ecs.count = 1;
-   ecs.ctrl_class = V4L2_CTRL_CLASS_CAMERA;
-
-	printf("%d\n",ioctl(fd, VIDIOC_G_EXT_CTRLS, &ecs)); 
-   printf("%lld %d\n",ec.value64,ec.value);
-   //ec.value = 100000;
-   ec.value64 = (long long int)10;
-   ec.size = 0;
-
-   
-
-   if(ioctl(fd, VIDIOC_S_EXT_CTRLS, &ecs) < 0)
-   {
-      printf("error in extended controls bitrate\n");
-      exit(0);
-   }
-
+        struct v4l2_ext_controls ecs;
+        struct v4l2_ext_control ec;
+        memset(&ecs, 0, sizeof(ecs));
+        memset(&ec, 0, sizeof(ec));
+        ec.id = 0x009a206f;
+        ecs.controls = &ec;
+        ecs.count = 1;
+        ecs.ctrl_class = V4L2_CTRL_CLASS_CAMERA;
+        xioctl(fd, VIDIOC_G_EXT_CTRLS, &ecs);
+        return ec.value64;
 }
 
-int main(int argc, char **argv)
+void set_analog_gain(int fd, int64_t value)
 {
-        struct v4l2_format              fmt;
-        struct v4l2_buffer              buf;
-        struct v4l2_requestbuffers      req;
-        enum v4l2_buf_type              type;
-        fd_set                          fds;
-        struct timeval                  tv;
-        int                             r, fd = -1;
-        unsigned int                    i, n_buffers;
-        char                            *dev_name = "/dev/video0";
-        char                            out_name[256];
-        FILE                            *fout;
-        struct buffer                   *buffers;
+        struct v4l2_ext_controls ecs;
+        struct v4l2_ext_control ec;
+        memset(&ecs, 0, sizeof(ecs));
+        memset(&ec, 0, sizeof(ec));
+        ec.id = 0x009a206f;
+        ecs.controls = &ec;
+        ecs.count = 1;
+        ecs.ctrl_class = V4L2_CTRL_CLASS_CAMERA;
+        ec.value64 = value;
+        ec.size = 0;
+        xioctl(fd, VIDIOC_S_EXT_CTRLS, &ecs);
+}
 
-        fd = /*v4l2_*/open(dev_name, O_RDWR | O_NONBLOCK, 0);
-        if (fd < 0) {
-                perror("Cannot open device");
-                exit(EXIT_FAILURE);
-        }
 
-        CLEAR(fmt);
-        fmt.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
-        fmt.fmt.pix.width       = 1920;
-        fmt.fmt.pix.height      = 1080;
-        fmt.fmt.pix.pixelformat = V4L2_PIX_FMT_GREY;
-        fmt.fmt.pix.field       = V4L2_FIELD_NONE;
-        xioctl(fd, VIDIOC_S_FMT, &fmt);
-        if (fmt.fmt.pix.pixelformat != V4L2_PIX_FMT_GREY) {
-                printf("Libv4l didn't accept GREY format. Can't proceed.\\n");
-                exit(EXIT_FAILURE);
-        }
-        if ((fmt.fmt.pix.width != 1920) || (fmt.fmt.pix.height != 1080))
-                printf("Warning: driver is sending image at %dx%d\\n",
-                        fmt.fmt.pix.width, fmt.fmt.pix.height);
 
-        CLEAR(req);
-        req.count = 2;
-        req.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
-        req.memory = V4L2_MEMORY_MMAP;
-        xioctl(fd, VIDIOC_REQBUFS, &req);
-
-        buffers = calloc(req.count, sizeof(*buffers));
-        for (n_buffers = 0; n_buffers < req.count; ++n_buffers) {
-                CLEAR(buf);
-
-                buf.type        = V4L2_BUF_TYPE_VIDEO_CAPTURE;
-                buf.memory      = V4L2_MEMORY_MMAP;
-                buf.index       = n_buffers;
-
-                xioctl(fd, VIDIOC_QUERYBUF, &buf);
-
-                buffers[n_buffers].length = buf.length;
-                buffers[n_buffers].start = /*v4l2_*/mmap(NULL, buf.length,
-                              PROT_READ | PROT_WRITE, MAP_SHARED,
-                              fd, buf.m.offset);
-
-                if (MAP_FAILED == buffers[n_buffers].start) {
-                        perror("mmap");
-                        exit(EXIT_FAILURE);
-                }
-        }
-
-        for (i = 0; i < n_buffers; ++i) {
-                CLEAR(buf);
-                buf.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
-                buf.memory = V4L2_MEMORY_MMAP;
-                buf.index = i;
-                xioctl(fd, VIDIOC_QBUF, &buf);
-        }
-        type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
-	setExpTime(fd,200000);
-        xioctl(fd, VIDIOC_STREAMON, &type);
-        for (i = 0; i < 5; i++) {
-                do {
+void capture_image(int i)
+{
+        int r;
+        struct timeval tv;
+        do
+                {
                         FD_ZERO(&fds);
                         FD_SET(fd, &fds);
-
                         /* Timeout. */
                         tv.tv_sec = 2;
                         tv.tv_usec = 0;
 
                         r = select(fd + 1, &fds, NULL, NULL, &tv);
                 } while ((r == -1 && (errno = EINTR)));
-                if (r == -1) {
+                if (r == -1)
+                {
                         perror("select");
                         return errno;
                 }
@@ -175,7 +107,8 @@ int main(int argc, char **argv)
 
                 sprintf(out_name, "out%03d.raw", i);
                 fout = fopen(out_name, "w");
-                if (!fout) {
+                if (!fout)
+                {
                         perror("Cannot open image");
                         exit(EXIT_FAILURE);
                 }
@@ -185,13 +118,109 @@ int main(int argc, char **argv)
                 fclose(fout);
 
                 xioctl(fd, VIDIOC_QBUF, &buf);
+}
+
+
+void capture_image_sequence(int fd, fd_set fds, v4l2_buffer buf)
+{
+        for (i = 0; i < 10; i++)
+        {
+                set_analog_gain(fd, i);
+                sleep(0.5);
+                capture_image(i);
+        }
+}
+
+int main(int argc, char **argv)
+{
+        struct v4l2_format fmt; //struct containing
+        struct v4l2_buffer buf;
+        struct v4l2_requestbuffers req;
+        enum v4l2_buf_type type;
+        fd_set fds;
+        
+        int fd = -1;
+        unsigned int i, n_buffers;
+        char *dev_name = "/dev/video0";
+        char out_name[256];
+        FILE *fout;
+        struct buffer *buffers;
+
+        fd = open(dev_name, O_RDWR | O_NONBLOCK, 0);
+        if (fd < 0)
+        {
+                perror("Cannot open device");
+                exit(EXIT_FAILURE);
         }
 
+        // initialize format 
+        CLEAR(fmt);
+        fmt.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+        fmt.fmt.pix.width = 1920;
+        fmt.fmt.pix.height = 1080;
+        fmt.fmt.pix.pixelformat = V4L2_PIX_FMT_GREY;
+        fmt.fmt.pix.field = V4L2_FIELD_NONE;
+
+        //apply format
+        xioctl(fd, VIDIOC_S_FMT, &fmt);
+        if (fmt.fmt.pix.pixelformat != V4L2_PIX_FMT_GREY)
+        {
+                printf("Libv4l didn't accept GREY format. Can't proceed.\\n");
+                exit(EXIT_FAILURE);
+        }
+        if ((fmt.fmt.pix.width != 1920) || (fmt.fmt.pix.height != 1080))
+                printf("Warning: driver is sending image at %dx%d\\n",
+                       fmt.fmt.pix.width, fmt.fmt.pix.height);
+
+
+        CLEAR(req);
+        req.count = 2;
+        req.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+        req.memory = V4L2_MEMORY_MMAP;
+        xioctl(fd, VIDIOC_REQBUFS, &req);
+
+        buffers = calloc(req.count, sizeof(*buffers));
+        for (n_buffers = 0; n_buffers < req.count; ++n_buffers)
+        {
+                CLEAR(buf);
+
+                buf.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+                buf.memory = V4L2_MEMORY_MMAP;
+                buf.index = n_buffers;
+
+                xioctl(fd, VIDIOC_QUERYBUF, &buf);
+
+                buffers[n_buffers].length = buf.length;
+                buffers[n_buffers].start = mmap(NULL, buf.length,
+                                                PROT_READ | PROT_WRITE, MAP_SHARED,
+                                                fd, buf.m.offset);
+
+                if (MAP_FAILED == buffers[n_buffers].start)
+                {
+                        perror("mmap");
+                        exit(EXIT_FAILURE);
+                }
+        }
+
+        for (i = 0; i < n_buffers; ++i)
+        {
+                CLEAR(buf);
+                buf.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+                buf.memory = V4L2_MEMORY_MMAP;
+                buf.index = i;
+                xioctl(fd, VIDIOC_QBUF, &buf);
+        }
         type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+        setExpTime(fd, 200000);
+        xioctl(fd, VIDIOC_STREAMON, &type);
+
+        capture_image_sequence(fd)
+
+            type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
         xioctl(fd, VIDIOC_STREAMOFF, &type);
         for (i = 0; i < n_buffers; ++i)
-                /*v4l2_*/munmap(buffers[i].start, buffers[i].length);
-        /*v4l2_*/close(fd);
+                munmap(buffers[i].start, buffers[i].length);
+        close(fd);
 
         return 0;
 }

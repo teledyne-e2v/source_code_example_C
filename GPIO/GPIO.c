69bdb3eeae5e9b00d82aa9455d7c0876dd9cb09b
loic <loic.chevallier@teledyne.com>
2023-06-29 13:31:28 +0200

    ISO 90 warning fixe

--- GPIO/GPIO.c
+++ GPIO/GPIO.c
@@ -8,22 +8,27 @@ You need to execute the program with sudo or you will get the error : Error: can
 #include <fcntl.h>
 #include <stdlib.h>
 
-
 static const char gpio_path[] = "/sys/class/gpio/";
+void unexport(char *pin);
+void export(char *pin);
+void direction(char *pin);
 
-void unexport(char* pin) // unexport pin (if not done pin will be unusable on next launch)
+void unexport(char *pin) // unexport pin (if not done pin will be unusable on next launch)
 {
 
-   char unexport[128];
-   strcpy(unexport,gpio_path);
-   strcat(unexport,"unexport");
-    int fd = open(unexport, O_WRONLY);
-    if (fd == -1) {
+    char unexport[128];
+    int fd;
+    strcpy(unexport, gpio_path);
+    strcat(unexport, "unexport");
+    fd = open(unexport, O_WRONLY);
+    if (fd == -1)
+    {
         perror("Error: cannot open the unexport file!");
         exit(EXIT_FAILURE);
     }
 
-    if (write(fd, pin, 2) != 2) {
+    if (write(fd, pin, 2) != 2)
+    {
         perror("Error: cannot write the unexport!");
         exit(EXIT_FAILURE);
     }
@@ -31,21 +36,23 @@ void unexport(char* pin) // unexport pin (if not done pin will be unusable on ne
     close(fd);
 }
 
-
-void export(char* pin) // Check and setup export
+void export(char *pin) // Check and setup export
 {
-   char export[128];
-   strcpy(export,gpio_path);
-   strcat(export,"export");
-   int fd = open(export, O_WRONLY);
+    char export[128];
+    int fd;
+    strcpy(export, gpio_path);
+    strcat(export, "export");
+    fd = open(export, O_WRONLY);
 
-    if (fd == -1) {
+    if (fd == -1)
+    {
         perror("Error: cannot open the export file! check permissions");
         exit(EXIT_FAILURE);
     }
 
-    if (write(fd, pin, 2) != 2) {
-	unexport(pin); // try to unexport the pin (fix the most common error, try to relaunch)
+    if (write(fd, pin, 2) != 2)
+    {
+        unexport(pin); // try to unexport the pin (fix the most common error, try to relaunch)
         perror("Error: cannot write to the export! pin is maybe busy try to relaunch");
         exit(EXIT_FAILURE);
     }
@@ -53,21 +60,23 @@ void export(char* pin) // Check and setup export
     close(fd);
 }
 
-
-void direction(char* pin)  // Check and setup direction to output
+void direction(char *pin) // Check and setup direction to output
 {
     char direction[128];
-    strcpy(direction,gpio_path);
-    strcat(direction,"gpio");
-    strcat(direction,pin);
-    strcat(direction,"/direction");
-    int fd = open(direction, O_WRONLY);
-    if (fd == -1) {
+    int fd;
+    strcpy(direction, gpio_path);
+    strcat(direction, "gpio");
+    strcat(direction, pin);
+    strcat(direction, "/direction");
+    fd = open(direction, O_WRONLY);
+    if (fd == -1)
+    {
         perror("Error: cannot open the direction file!");
         exit(EXIT_FAILURE);
     }
 
-    if (write(fd, "out", 3) != 3) {
+    if (write(fd, "out", 3) != 3)
+    {
         perror("Error: cannot write the direction!");
         exit(EXIT_FAILURE);
     }
@@ -75,48 +84,49 @@ void direction(char* pin)  // Check and setup direction to output
     close(fd);
 }
 
-
-
-int main()
+int main(void)
 {
-	char high[2] = "1";
-	char low[2] = "0";
-	char pin[4]="64";
+    char high[2] = "1";
+    char low[2] = "0";
+    char pin[4] = "64";
+    int fd;
+    
+    float frequency = 100; // frequency of 100hz
 
-	char value[128]; // the value file is where we have to change the GPIO value (write 1 will outpout GPIO tension of 3.3V 0 will output tension of 0V)
-	strcpy(value,gpio_path);
-	strcat(value,"gpio");
-	strcat(value,pin);
-	strcat(value,"/value");
-	export(pin);
-    	direction(pin);
-	
+    char value[128]; // the value file is where we have to change the GPIO value (write 1 will outpout GPIO tension of 3.3V 0 will output tension of 0V)
+    strcpy(value, gpio_path);
+    strcat(value, "gpio");
+    strcat(value, pin);
+    strcat(value, "/value");
+    export(pin);
+    direction(pin);
 
-    int fd = open(value, O_WRONLY);
-    if (fd == -1) {
+    fd = open(value, O_WRONLY);
+    if (fd == -1)
+    {
         perror("Error: cannot open the value file!");
         exit(EXIT_FAILURE);
     }
 
-    float frequency=100; //frequency of 100hz
 
-    for (int i = 0; i < 1000; i++) {
-        if (write(fd, high, 1) != 1) {
+    for (int i = 0; i < 1000; i++)
+    {
+        if (write(fd, high, 1) != 1)
+        {
             perror("Error: cannot write the value!");
             exit(EXIT_FAILURE);
         }
-        usleep(1000000/(2* frequency));
+        usleep(1000000 / (2 * frequency));
 
-        if (write(fd, low, 1) != 1) {
+        if (write(fd, low, 1) != 1)
+        {
             perror("Error: cannot write the value!");
             exit(EXIT_FAILURE);
         }
-        usleep(1000000/(2* frequency));
+        usleep(1000000 / (2 * frequency));
     }
     unexport(pin);
     close(fd);
-
-   
 
     return 0;
 }
